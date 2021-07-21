de0077a7e7bf41000b5d7d8ab7e130bc1f174c3b
cristobal hernandez chavez <cristobal.hernandez@ilumps.com>
2021-07-21 17:09:28 -0600

    Add questions, timer and score

--- Assets/JS/script.js
+++ Assets/JS/script.js
@@ -10,39 +10,99 @@ const choiceC = document.getElementById("C")
 const choiceD = document.getElementById("D")
 const progress = document.getElementById("progress")
 const score = document.getElementById("score")
+const wrongText = document.getElementById("wrongText")
+const hiScore = document.getElementById("hiScore")
 
 // List of questions, options and answers
 let questions = [
     {
-        question: "What is 2+2?",
-        choiceA: "1",
-        choiceB: "1",
-        choiceC: "4",
-        choiceD: "1",
+        question: "Which of the following best describes what a function in JS is used for",
+        choiceA: "A function creates new variables",
+        choiceB: "A function is a reusable piece of code that can accept input and performs a specific task",
+        choiceC: "A function stores data",
+        choiceD: "A function allows for the use of mathematical operators",
+        correct: "B"
+    },
+    {
+        question: "What is the purpose of a parameter?",
+        choiceA: "To call a function",
+        choiceB: "To specify actual values passed to a function",
+        choiceC: "To allow a function to accept data",
+        choiceD: "None of the above",
         correct: "C"
     },
     {
-        question: "What is 2+2?",
-        choiceA: "4",
-        choiceB: "1",
-        choiceC: "1",
-        choiceD: "1",
+        question: "Using 'var' is the same as using 'let'?",
+        choiceA: "Yes",
+        choiceB: "No",
+        choiceC: "Depends on the context",
+        choiceD: "You said what now?",
         correct: "A"
     },
     {
-        question: "What is 2+2?",
-        choiceA: "1",
-        choiceB: "1",
-        choiceC: "1",
-        choiceD: "4",
+        question: " Which of the following is a JS data type ",
+        choiceA: "null",
+        choiceB: "undefined",
+        choiceC: "object",
+        choiceD: "All of the above",
         correct: "D"
     },
+    {
+        question: "What are the three important manipulations done in a for loop on a loop variable? ",
+        choiceA: "Initialization,Testing, Incrementation",
+        choiceB: "Updation, Incrementation, Initialization",
+        choiceC: "Initialization,Testing, Updation",
+        choiceD: "Testing, Updation, Testing",
+        correct: "C"
+    },
+    {
+        question: "Which of the following is syntactically correct for loop?",
+        choiceA: "for (1<=10,i++)",
+        choiceB: "for i=1 to 10",
+        choiceC: "for (i=0;i<=10;i++)",
+        choiceD: "for (i=0;i<=10)",
+        correct: "C"
+    },
+    {
+        question: "The statement is an example of 'while (3==3) {}' ",
+        choiceA: "A typographical error",
+        choiceB: "An infinite loop",
+        choiceC: "An illegal JS statement",
+        choiceD: "None of the above",
+        correct: "B"
+    },
+    {
+        question: "What would be the output of'console.log(typeof('5' + 5 ))?  ",
+        choiceA: "number",
+        choiceB: "string",
+        choiceC: "null",
+        choiceD: "object",
+        correct: "B"
+    },
+    {
+        question: "Which operator is known as the equality operator; which checks whether its two operators are 'equal'?",
+        choiceA: "=",
+        choiceB: "==",
+        choiceC: "===",
+        choiceD: "&&",
+        correct: "B"
+    },
+    {
+        question: "If an operator is NaN or converts to NaN, what will the comparison operator return?",
+        choiceA: "True",
+        choiceB: "False",
+        choiceC: "Undefined",
+        choiceD: "NaN",
+        correct: "B"
+    },
 ];
+
 
 //Questions render
 let lastQuestion = questions.length - 1;
 let runningQuestion = 0; //Current Quesiton
 let timeLeft = 60; //Time left on quiz
+let quizScore = 0 //Number of correct answers
 
 function renderQuestion() {
     let q = questions[runningQuestion];
@@ -64,19 +124,18 @@ function startQuiz() { //Notes: DONT USE ARROW FUNCTIONS WHEN ADDING EVENT LISTE
     renderCounter();
 }
 
-startQuiz(); //NOTE: REMOVE AFTER TESTING
 
 
 // render progress
 function renderProgress() {
     for (let qIndex = 0; qIndex <= lastQuestion; qIndex++) {
-        progress.innerHTML = "<div><h1>" + qIndex + "/" + questions.length + "</h1></div>"
+        questionNumber.innerHTML = qIndex + " / " + questions.length
     }
 };
 
 //render time counter
 function renderCounter() {
-    
+
     let timerInterval = setInterval(function () {
         timeLeft--;
         counter.textContent = timeLeft
@@ -86,6 +145,29 @@ function renderCounter() {
             counter.textContent = "Time is up" //NOTE: MAKE IT END THE PROGRAM
         }
     }, 1000);
+};
+
+//Check Answers
+function checkAnswer(answer) {
+    if (answer == questions[runningQuestion].correct) {
+        quizScore++; //Correct answer
+        if(runningQuestion <= lastQuestion) {
+            runningQuestion++;
+            renderQuestion();
+            score.innerHTML = "<h1>" + quizScore + "</h1>"
+        }
+    } else {
+        timeLeft = timeLeft - 5
+        //Wrong answer text show
+        wrongText.textContent = "Wrong answer, 5 seconds have been subtracted"
+        wrongText.style.color = "White"
+        let wrongTextTime = 0
+
+        let wrongTextTimerInterval = setInterval(function () {
+            wrongTextTime++;
+            if (wrongTextTime === 2) {
+                wrongText.style.color="transparent"
+            }
+        }, 1000)
+    }
 }
-
-
